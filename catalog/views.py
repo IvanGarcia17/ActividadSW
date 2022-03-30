f3a561f2230a499853751708adf2f95ac64dcb90
IvanGarcia17 <theivan_garcia17@hotmail.com>
2022-03-30 13:20:05 -0600

    4

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
+from django.contrib.auth.mixins import PermissionRequiredMixin
+import datetime
+from .forms import RenewBookForm
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
 
